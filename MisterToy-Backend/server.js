11990dead7d7c973e8e4def7dd6a4af08e5c39a2
Moria Hamami <moriahamami@gmail.com>
2023-01-10 18:59:03 +0200

    Change middleware

--- MisterToy-Backend/server.js
+++ MisterToy-Backend/server.js
@@ -6,13 +6,23 @@ const cors = require('cors')
 const app = express()
 
 // App configuration
-app.use(express.static('public'))
-
-const corsOptions = {
-    origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
-    credentials: true
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+    const corsOptions = {
+        origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+        credentials: true
+    }
+    app.use(cors(corsOptions))
 }
-app.use(cors(corsOptions))
+
+// app.use(express.static('public'))
+
+// const corsOptions = {
+//     origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
+//     credentials: true
+// }
+// app.use(cors(corsOptions))
 
 
 app.use(cookieParser())
@@ -124,7 +134,7 @@ app.delete('/api/toy/:toyId', (req, res) => {
 
 
 // app.post('/api/user/login', (req, res) => {
-  
+
 //     const { username, password } = req.body
 //     userService.login({ username, password })
 //         .then((user) => {
@@ -158,7 +168,15 @@ app.delete('/api/toy/:toyId', (req, res) => {
 // })
 
 // // Listen will always be the last line in our server!
-app.listen(3030, () => console.log('Server listening on port 3030!'))
+// app.listen(3030, () => console.log('Server listening on port 3030!'))
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
+const port = process.env.PORT || 3030
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}!`)
+});
 
 
