9be5f88edef5e87001526fc4a6e73a79290fee95
Moria Hamami <moriahamami@gmail.com>
2023-03-15 17:58:37 +0200

    Update prod data source

--- MisterToy-Backend/config/prod.js
+++ MisterToy-Backend/config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-  dbName: 'toyDB'
+  dbURL: 'mongodb+srv://mori:mori@cluster0.nwvypal.mongodb.net/?retryWrites=true&w=majority',
+  dbName: 'ToyDB'
 }
